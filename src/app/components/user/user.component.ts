7bebec36da62fa14cd0039f491f074029de63322
NguyenLeAnh2002 <nguyenleanh2002@gmail.com>
2023-04-24 10:50:28 +0700

    generate front end for sing page 1st

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent {
+  isEdit: boolean = false;
+
+  onEdit() {
+    this.isEdit = true;
+  }
+
+
+  
+  isSave: boolean = false;
+  onSave(){
+    this.isSave = true; 
+  }
 
 }
