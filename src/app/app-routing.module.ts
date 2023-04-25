4e722a18a186f4438761f7ab0189627284812c04
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-04-25 20:41:22 +0700

    update routing for le anh

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LibraryComponent } from './components/library/library.component';
 import { UserComponent } from './components/user/user.component';
+import { SingpageComponent } from './components/singpage/singpage.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -12,6 +13,8 @@ const routes: Routes = [
   {path: 'signup', component: SignupComponent},
   {path: 'library', component: LibraryComponent},
   {path: 'user', component: UserComponent},
+  {path: 'singpage', component: SingpageComponent},
+  
 ];
 
 @NgModule({
