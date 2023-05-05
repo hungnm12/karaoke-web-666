a4ecc9ef04f805e3fbf99afda9452d138087052e
NguyenLeAnh2002 <nguyenleanh2002@gmail.com>
2023-05-05 10:35:20 +0700

    singpage update

--- src/app/services/video services/video.service.ts
+++ src/app/services/video services/video.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,13 @@ export class VideoService {
 
   constructor(private httpClient: HttpClient) { }
 
-  
+  uploadVideo(fileEntry: File){
+    const formData = new FormData()
+          formData.append('file', fileEntry, fileEntry.name)
+    //http post call
+    
+    
+    
+    return this.httpClient.post("http://localhost:8080/api/video/ng ", formData);
+  }
 }
