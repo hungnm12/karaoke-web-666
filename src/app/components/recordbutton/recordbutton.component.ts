cb97c392efcfed3a30e218dd11b5d3ce0c4fcfaf
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-05-08 20:55:44 +0700

    comment code to clear errors

--- src/app/components/recordbutton/recordbutton.component.ts
+++ src/app/components/recordbutton/recordbutton.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Recorder } from 'mic-recorder-to-mp3';
+
 import 'webrtc-adapter';
 @Component({
   selector: 'app-recordbutton',
@@ -9,41 +9,41 @@ import 'webrtc-adapter';
 })
 export class RecordbuttonComponent {
 
-  private mediaStream: MediaStream;
-  private recordRTC: RecordRTC;
-  private audioBlob: Blob;
-  public recording = false;
+  // private mediaStream: MediaStream;
+  // private recordRTC: RecordRTC;
+  // private audioBlob: Blob;
+  // public recording = false;
 
-  public onRecord() {
-    if (this.recording) {
-      this.stopRecording();
-      this.recording = false;
-    } else {
-      this.startRecording();
-      this.recording = true;
-    }
-  }
+  // public onRecord() {
+  //   if (this.recording) {
+  //     this.stopRecording();
+  //     this.recording = false;
+  //   } else {
+  //     this.startRecording();
+  //     this.recording = true;
+  //   }
+  // }
 
-  public onPlay() {
-    const audioUrl = URL.createObjectURL(this.audioBlob);
-    const audioElement = new Audio(audioUrl);
-    audioElement.play();
-  }
+  // public onPlay() {
+  //   const audioUrl = URL.createObjectURL(this.audioBlob);
+  //   const audioElement = new Audio(audioUrl);
+  //   audioElement.play();
+  // }
 
-  private startRecording() {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
-      this.mediaStream = stream;
-      this.recordRTC = RecordRTC(stream, {
-        type: 'audio',
-      });
-      this.recordRTC.startRecording();
-    });
-  }
+  // private startRecording() {
+  //   navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+  //     this.mediaStream = stream;
+  //     this.recordRTC = RecordRTC(stream, {
+  //       type: 'audio',
+  //     });
+  //     this.recordRTC.startRecording();
+  //   });
+  // }
 
-  private stopRecording() {
-    this.recordRTC.stopRecording(() => {
-      this.audioBlob = this.recordRTC.getBlob();
-      this.mediaStream.getTracks().forEach((track) => track.stop());
-    });
-  }
+  // private stopRecording() {
+  //   this.recordRTC.stopRecording(() => {
+  //     this.audioBlob = this.recordRTC.getBlob();
+  //     this.mediaStream.getTracks().forEach((track) => track.stop());
+  //   });
+  // }
 }
