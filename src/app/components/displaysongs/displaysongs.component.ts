e6c925c3d9dfde2adb13a56d22135741da827d92
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-05 23:44:16 +0700

    update

--- src/app/components/displaysongs/displaysongs.component.ts
+++ src/app/components/displaysongs/displaysongs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-displaysongs',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./displaysongs.component.css']
 })
 export class DisplaysongsComponent {
+  songs = [
+    {
+      id: '1', // Unique identifier for the song
+      title: 'Song 1',
+      artist: 'Artist 1',
+      duration: '3:30',
+      imageUrl: 'path/to/song1-image.jpg',
+      videoUrl: 'path/to/song1-video.mp4',
+    },
+    {
+      id: '2',
+      title: 'Song 2',
+      artist: 'Artist 2',
+      duration: '4:15',
+      imageUrl: 'path/to/song2-image.jpg',
+      videoUrl: 'path/to/song2-video.mp4',
+    },
+    {
+      id: '3',
+      title: 'Song 3',
+      artist: 'Artist 3',
+      duration: '2:50',
+      imageUrl: 'path/to/song3-image.jpg',
+      videoUrl: 'path/to/song3-video.mp4',
+    },
+    // Add more songs as needed
+  ];
+  
+  
+  selectedSong: any; // Variable to store the selected song
 
+  constructor(private router: Router) {}
+
+  selectSong(song: any): void {
+    this.router.navigate(['/song', song.id]);
+  }
 }
--- src/app/components/singpage/singpage.component.ts
+++ src/app/components/singpage/singpage.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-singpage',
   templateUrl: './singpage.component.html',
   styleUrls: ['./singpage.component.css']
 })
-export class SingpageComponent {
+export class SingpageComponent implements OnInit {
 
+  selectedSong: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const songId = params.get('id');
+      if (songId) {
+        this.selectedSong = this.getSongById(songId);
+      }
+    });
+  }
+
+  getSongById(id: string): any {
+    // Implement your logic to retrieve the selected song based on the ID
+    return {
+      id: '1',
+      title: 'Song 1',
+      artist: 'Artist 1',
+      duration: '3:30',
+      imageUrl: 'path/to/song1-image.jpg',
+      videoUrl: 'path/to/song1-video.mp4',
+    };
+  }
 
 }
