79b66284dd26f0b34cfbf2ee504fe438dfa1885c
tai110402 <tail110402@gmail.com>
2023-07-06 11:21:52 +0700

    library

--- src/app/components/displaysongs/displaysongs.component.ts
+++ src/app/components/displaysongs/displaysongs.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
   templateUrl: './displaysongs.component.html',
   styleUrls: ['./displaysongs.component.css']
 })
+
 export class DisplaysongsComponent {
   songs = [
     {
@@ -18,6 +19,30 @@ export class DisplaysongsComponent {
     },
     {
       id: '2',
+      title: 'Thu Cuối',
+      artist: 'Mr T x Yanbi x Hằng Bingboong',
+      duration: '5p',
+      imageUrl: '../../../assets/imgs/thucuoi.jpg',
+      videoUrl: '../../../assets/imgs/videohome-thucuoi.mp4',
+    },
+    {
+      id: '3',
+      title: 'Đào Nương',
+      artist: 'Hoàng Vương',
+      duration: '3p',
+      imageUrl: '../../../assets/imgs/daonuong.png',
+      videoUrl: '../../../assets/imgs/videohome-daonuong.mp4',
+    },
+    {
+      id: '4', // Unique identifier for the song
+      title: 'Gap Me Trong Mo',
+      artist: 'Nothing',
+      duration: '9p',
+      imageUrl: '../../../assets/imgs/disc1.jpg',
+      videoUrl: '../../../assets/imgs/videohome2.mp4',
+    },
+    {
+      id: '5',
       title: 'That Girl',
       artist: 'XOXXX',
       duration: '4:15',
@@ -25,7 +50,55 @@ export class DisplaysongsComponent {
       videoUrl: '../../../assets/imgs/videohome1.mp4',
     },
     {
-      id: '3',
+      id: '6',
+      title: 'Roller Coaster',
+      artist: 'NMIXX',
+      duration: '2:50',
+      imageUrl: '../../../assets/imgs/music_background.jpg',
+      videoUrl: '../../../assets/imgs/videohome1.mp4',
+    },
+    {
+      id: '7', // Unique identifier for the song
+      title: 'Gap Me Trong Mo',
+      artist: 'Nothing',
+      duration: '9p',
+      imageUrl: '../../../assets/imgs/disc1.jpg',
+      videoUrl: '../../../assets/imgs/videohome2.mp4',
+    },
+    {
+      id: '8',
+      title: 'That Girl',
+      artist: 'XOXXX',
+      duration: '4:15',
+      imageUrl: '../../../assets/imgs/imgtest.png',
+      videoUrl: '../../../assets/imgs/videohome1.mp4',
+    },
+    {
+      id: '9',
+      title: 'Roller Coaster',
+      artist: 'NMIXX',
+      duration: '2:50',
+      imageUrl: '../../../assets/imgs/music_background.jpg',
+      videoUrl: '../../../assets/imgs/videohome1.mp4',
+    },
+    {
+      id: '10', // Unique identifier for the song
+      title: 'Gap Me Trong Mo',
+      artist: 'Nothing',
+      duration: '9p',
+      imageUrl: '../../../assets/imgs/disc1.jpg',
+      videoUrl: '../../../assets/imgs/videohome2.mp4',
+    },
+    {
+      id: '11',
+      title: 'That Girl',
+      artist: 'XOXXX',
+      duration: '4:15',
+      imageUrl: '../../../assets/imgs/imgtest.png',
+      videoUrl: '../../../assets/imgs/videohome1.mp4',
+    },
+    {
+      id: '12',
       title: 'Roller Coaster',
       artist: 'NMIXX',
       duration: '2:50',
