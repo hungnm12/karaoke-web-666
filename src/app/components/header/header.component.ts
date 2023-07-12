35457c9114cdbc403e9e2b27164158cf1f106c9e
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-12 23:08:08 +0700

    lilking

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
+import { Song, SongService } from 'src/app/song.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  query: string = '';
+  songs: Song[] = [];
 
+  constructor(private authService: AuthService, private songService: SongService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  search(): void {
+    if (this.query.trim()) {
+      this.songService.searchSongs(this.query)
+        .subscribe(songs => this.songs = songs);
+    } else {
+      this.songs = [];
+    }
+  }
+
+  goToSong(song: Song): void {
+    this.router.navigate(['/song', song.id]);
+  }
 }
--- src/app/song.service.ts
+++ src/app/song.service.ts
@@ -18,90 +18,42 @@ export interface Song {
   })
   export class SongService {
     private apiURL = 'http://localhost:8002/api/songs';
-    songs: Song[] = [
-      {
-        id: '1',
-        title: 'Thang tu la loi noi doi cua em',
-        artist: 'Ha Anh Tune',
-      
-        imageUrl: '../../../assets/imgs/Áp_phích_phim_Tháng_Tư_là_lời_nói_dối_của_em.jpg',
-        videoUrl: '../../../assets/imgs/videohome3.mp4',
-        genre: 'Ballad'
-      },
-      {
-        id: '2',
-        title: 'Flower',
-        artist: 'Huan Hoa Tu',
-        
-        imageUrl: '../../../assets/imgs/imgtest.png',
-        videoUrl: '../../../assets/imgs/flower666.mp4',
-        genre: 'Pop'
-      },
-      {
-        id: '3',
-        title: 'Thu Cuoi',
-        artist: 'Hang Bing Bong,Mr A, Yanbi',
-        
-        imageUrl: '../../../assets/imgs/thucuoi.jpg',
-        videoUrl: '../../../assets/imgs/videohome-thucuoi.mp4',
-        genre: 'Pop'
-      },
-     
-     
-    ];
+  
 
     constructor(private http: HttpClient, private authService: AuthService) { }
   
     getAllSongs(): Observable<Song[]> {
       return this.http.get<Song[]>(`${this.apiURL}/getAllSongs`);
     }
-
-    // addSong(song: Song, imageFile: File, videoFile: File): Observable<Song> {
-    //   const formData = new FormData();
-    //   formData.append('title', song.title);
-    //   formData.append('artist', song.artist);
-    //   formData.append('genre', song.genre);
-     
-    //   formData.append('image', imageFile);
-    //   formData.append('video', videoFile);
-    //   return this.http.post<Song>(this.apiURL, formData);
-    // }
-
-    addSong(song: Song, imageFile: File, videoFile: File): Observable<Song> {
+  
+    getSongById(id: string): Observable<Song> {
+      return this.http.get<Song>(`${this.apiURL}/getSong/${id}`);
+    }
+  
+    createSong(song: Song, imageFile: File, videoFile: File): Observable<Song> {
       const formData = new FormData();
       formData.append('title', song.title);
       formData.append('artist', song.artist);
       formData.append('genre', song.genre);
-      
       formData.append('image', imageFile);
       formData.append('video', videoFile);
-    
-      const headers = new HttpHeaders({
-        Authorization: 'Bearer ' + this.authService.getToken()
-      });
-    
-      return this.http.post<Song>(this.apiURL, formData, { headers });
+      return this.http.post<Song>(`${this.apiURL}/add`, formData);
     }
-    updateSong(id: string, image: File | null, video: File | null, song: Object): Observable<any> {
-      const formData = new FormData();
-      if (image) {
-        formData.append('image', image);
-      }
-      if (video) {
-        formData.append('video', video);
-      }
-      formData.append('song', JSON.stringify(song));
-      const headers = new HttpHeaders();
-      headers.append('Content-Type', 'multipart/form-data');
-      return this.http.put<any>(`${this.apiURL}/update/${id}`, formData, { headers });
+  
+    updateSong(id: string, song: Song): Observable<Song> {
+      return this.http.put<Song>(`${this.apiURL}/update/${id}`, song);
     }
-
+  
     deleteSong(id: string): Observable<any> {
-      return this.http.delete<any>(`${this.apiURL}/delete/${id}`);
+      return this.http.delete(`${this.apiURL}/delete/${id}`);
     }
-    
-    searchSongs(title: string, artist: string): Observable<Song[]> {
-      const params = new HttpParams().set('title', title).set('artist', artist);
+    getSongsByGenre(genre: string): Observable<Song[]> {
+      const url = `${this.apiURL}/?genre=${genre}`;
+      return this.http.get<Song[]>(url);
+    }
+    searchSongs(query: string): Observable<Song[]> {
+      const params = new HttpParams().set('q', query);
       return this.http.get<Song[]>(`${this.apiURL}/search`, { params });
-    }
   }
+  }
+  
