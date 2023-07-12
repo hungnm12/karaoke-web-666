eb3b670160185a5230cf0ddf1a41d09f69ecebbd
Mạnh Hùng <81520197+hungnm12@users.noreply.github.com>
2023-07-12 22:48:48 +0700

    update ft12

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SongService, Song } from 'src/app/song.service';
+import { ActivatedRoute } from '@angular/router';
 
-interface Genre {
-  name: string;
-  songs: Song[];
-}
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
@@ -12,18 +9,22 @@ interface Genre {
 })
 export class LibraryComponent implements OnInit {
   songs: Song[] = [];
-  genres: Genre[] = [
-    
-    { name: 'Ballad', songs: [] },
-    { name: 'Blue', songs: [] },
-    { name: 'Pop', songs: [] },
-    { name: 'Rap', songs: [] }
-  ];
-  constructor(private songService: SongService) { }
+  genre: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private songService: SongService
+  ) { }
 
   ngOnInit(): void {
-    
+    this.genre = this.route.snapshot.paramMap.get('genre');
+    if (this.genre) {
+      this.songService.getSongsByGenre(this.genre)
+        .subscribe(songs => this.songs = songs);
+    } else {
+      this.songService.getAllSongs()
+        .subscribe(songs => this.songs = songs);
+    }
   }
-
   
 }
