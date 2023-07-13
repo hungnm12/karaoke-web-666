2af80f25e9ff736b793b7bce9beda57da4e0e420
LeThanhDat <89300362+lethanhdathust@users.noreply.github.com>
2023-07-13 11:17:39 +0700

    libraary

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -10,14 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 export class LibraryComponent implements OnInit {
   songs: Song[] = [];
   genre: string | null = null;
-
+  pop:Song[]=[];
+  rap:Song[] = [];
+  ballad:Song[] = [];
+  hiphop:Song[] = [];
   constructor(
     private route: ActivatedRoute,
     private songService: SongService
   ) { }
 
-  ngOnInit(): void {
-    this.genre = this.route.snapshot.paramMap.get('genre');
+  async ngOnInit(): Promise<void> {
+  
+    
+    this.genre =await this.route.snapshot.paramMap.get('genre');
     if (this.genre) {
       this.songService.getSongsByGenre(this.genre)
         .subscribe(songs => this.songs = songs);
@@ -25,6 +30,27 @@ export class LibraryComponent implements OnInit {
       this.songService.getAllSongs()
         .subscribe(songs => this.songs = songs);
     }
+   await this.getSongByGenre()
   }
-  
+  getSongByGenre(){
+    for(let i = 0; i < this.songs.length; i)
+    {
+      if(this.songs[i].genre.toLowerCase()=="pop")
+      {
+        this.pop.push(this.songs[i])
+
+      }
+      else if(this.songs[i].genre.toLowerCase()=="rap")
+      {
+        this.rap.push(this.songs[i])
+      }
+      else if(this.songs[i].genre.toLowerCase()=="ballad")
+      {
+        this.ballad.push(this.songs[i])
+      }
+      else {
+        this.hiphop.push(this.songs[i])
+      }
+    }
+  }
 }
